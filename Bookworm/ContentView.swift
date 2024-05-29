de7c06f571bca2a73f87841a46e610568c19d708
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-29 14:39:25 -0700

    Apply AddBookView and set up SwiftData

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -5,17 +5,30 @@
 //  Created by Dechon Ryan on 5/29/24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+    
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Text("Count: \(books.count)")
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Add Book", systemImage: "plus") {
+                            showingAddScreen.toggle()
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen, content: {
+                    AddBookView()
+                })
         }
-        .padding()
     }
 }
 
