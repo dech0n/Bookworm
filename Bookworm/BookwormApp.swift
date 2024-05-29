d7ec5e4a7e2c8f52883d0eabdc893200ae9e1938
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-29 12:44:39 -0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Dechon Ryan on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
