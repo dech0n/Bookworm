c3ba422db57abb209cba9317ba5bc47025e60b2d
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-29 14:27:24 -0700

    Create layout for Add Book form

---
+++ Bookworm/AddBook.swift
@@ -0,0 +1,58 @@
+//
+//  AddBook.swift
+//  Bookworm
+//
+//  Created by Dechon Ryan on 5/29/24.
+//
+
+import SwiftUI
+
+struct AddBook: View {
+    @Environment(\.modelContext) var modelContext
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = "Fantasy"
+    @State private var review = ""
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+                    
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section("Write a review") {
+                    TextEditor(text: $review)
+                    
+                    Picker("Rating", selection: $rating){
+                        ForEach(0..<6) {
+                            Text(String($0))
+                        }
+                    }                    
+                }
+                
+                Section {
+                    Button("Save") {
+                        // add the book
+                    }
+                }
+            }
+            .navigationTitle("Add Book")
+        }
+    }
+}
+
+#Preview {
+    AddBook()
+}
