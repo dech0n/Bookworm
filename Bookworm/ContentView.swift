e9ed7832af369c657814467ad5a6eab84d2a2978
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-31 12:12:18 -0700

    Make background red for 1-star books

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -35,6 +35,7 @@ struct ContentView: View {
                             }
                         }
                     }
+                    .listRowBackground(book.rating == 1 ? Color.red : nil)
                 }
                 .onDelete(perform: deleteBooks)
             }
